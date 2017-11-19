7260bd38f1f0583fa97a01929dbcb5e9963ec3f7
David Puerto <me@davidpuerto.com>
2017-11-19 00:32:13 -0800

    combining reducers

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
-import { firebaseApp } from './firebase';
-import { logUser } from './actions';
-import reducer from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { Router, Route, browserHistory } from "react-router";
+import { firebaseApp } from "./firebase";
+import { logUser } from "./actions";
+import reducer from "./reducers";
 
-import App from './components/App';
-import SignIn from './components/SignIn/SignIn';
-import SignUp from './components/SignUp/SignUp';
+import App from "./components/App";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 
 const store = createStore(reducer);
 
 firebaseApp.auth().onAuthStateChanged(user => {
-  if(user) {
-    console.log('user has signed in or up', user);
+  if (user) {
+    //console.log('user has signed in or up', user);
     const { email } = user;
-    store.dispatch(logUser(email))
-    browserHistory.push('/app');
+    store.dispatch(logUser(email));
+    browserHistory.push("/app");
   } else {
-    console.log('user has signed out or still needs to sign in', user);
-    browserHistory.replace('/signin');
+    //console.log('user has signed out or still needs to sign in', user);
+    browserHistory.replace("/signin");
   }
 });
 
@@ -32,5 +32,6 @@ ReactDOM.render(
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
     </Router>
-  </Provider>, document.getElementById('root')
-)
+  </Provider>,
+  document.getElementById("root")
+);
